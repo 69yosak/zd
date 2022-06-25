db97104fe9ba1ade231a628ea479c998251599cd
69yosak <a9610@yandex.ru>
2022-06-25 16:14:15 +0300

    test

--- tmp.cs
+++ tmp.cs
@@ -462,7 +462,7 @@ class HelloWorld
 
     public static string TicketToString(List<string>ticket)
     {
-        return typeSeats[int.Parse(ticket[4])] + "," + ticket[5] + " (Места)," + ticket[2] + "," + ticket[3];
+        return typeSeats[int.Parse(ticket[4])] + "," + "Места: " + ticket[5]+ "," + ticket[2] + "," + ticket[3];
     }
 
     public static void doLikeCLient()
